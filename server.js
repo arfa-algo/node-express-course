8e2ab7b898f346b90cc3980ace581c763e68051e
arfa-algo <88434403+arfa-algo@users.noreply.github.com>
2022-02-08 01:01:47 +0500

    add login POST route

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
+
 //The first line gives you access to the express library by searching your node_modules for "express". 
 const express = require('express');
+
 // The next creates an instance of the express constructor, which we will name "app".
 const app = express();
+
+//access "body parser" and make an insta
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData=[
 	{name:'Mark'},
@@ -23,6 +30,27 @@ app.get('/users/:id',function(req,res){
 		message: 'got one user',
 		user: req.params.id
 	})
+})
+
+app.post('/login',function(req,res){
+    const username=req.body.username;
+    const password=req.body.password;
+
+    const mockUsername="billyTheKid";
+    const mockPassword="superSecret";
+
+    if (username===mockUsername && password===mockPassword){
+         res.json({
+              success: true,
+              message: 'password and username match!',
+              token: 'encrypted token goes here'
+         })
+    } else {
+         res.json({
+              success: false,
+              message: 'password and username do not match'
+         })
+    }
 })
 
 //The app.listen method will start up the server locally on the port you give as its first argument (in this case the base url is: http://localhost:8000)
