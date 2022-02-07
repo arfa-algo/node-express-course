91726c53550e9f90c112c949086910e7d196c8c4
arfa-algo <88434403+arfa-algo@users.noreply.github.com>
2022-02-07 23:16:55 +0500

    add first GET route

--- server.js
+++ server.js
@@ -3,6 +3,19 @@ const express = require('express');
 // The next creates an instance of the express constructor, which we will name "app".
 const app = express();
 
+const mockUserData=[
+	{name:'Mark'},
+	{name:'Jill'}
+]
+
+app.get('/users',function(req,res){
+	res.json({
+		success: true,
+		message: 'successfully got users. Nice!',
+		users: mockUserData
+	})
+})
+
 //The app.listen method will start up the server locally on the port you give as its first argument (in this case the base url is: http://localhost:8000)
 app.listen(8000,function(){
     console.log("server is running")
